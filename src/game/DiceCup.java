bdafc6a82d9bbe93c04b5401af55ec7b039c48c5
wulff-dtu <s185036@student.dtu.dk>
2018-11-06 16:33:38 +0100

    Har tilføjet noget til Game's constructor, og lidt i DiceCup og Die

--- src/game/DiceCup.java
+++ src/game/DiceCup.java
@@ -1,4 +1,21 @@
 package game;
 
 public class DiceCup {
+
+    private Die[] dice;
+    private int sum;
+
+    public DiceCup(int numOfDice) {
+        dice = new Die[numOfDice];
+        for(int i = 0; i < numOfDice; i++) {
+            dice[i] = new Die(6);
+        }
+    }
+
+    //TODO add throwDice method.
+
+
+    public int getSum() {
+        return sum;
+    }
 }
--- src/game/Die.java
+++ src/game/Die.java
@@ -1,4 +1,13 @@
 package game;
 
 public class Die {
+
+    private int sides;
+
+    public Die(int sides) {
+        this.sides = sides;
+    }
+
+    // TODO write logic of the die.
+
 }
