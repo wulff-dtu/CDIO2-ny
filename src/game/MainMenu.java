ee6c38897478130239fcfef5165f8a6d8c12bfec
mabrkr <s185039@student.dtu.dk>
2018-11-06 14:36:17 +0100

    Ikke rigtig noget

--- src/game/MainMenu.java
+++ src/game/MainMenu.java
@@ -3,6 +3,8 @@ package game;
 public class MainMenu {
 
     public void displayMenu() {
+        Language langauage = new Language("english");
+        System.out.println();
         System.out.println();
     }
 }
