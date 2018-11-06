cbdff4fde9eba081a3499dc67909c986a0cee1d5
Mads <hemermads@gmail.com>
2018-11-06 18:52:06 +0100

    Die

--- src/game/Die.java
+++ src/game/Die.java
@@ -1,4 +1,20 @@
 package game;
 
+import java.util.Random;
+
 public class Die {
+    private int value;
+    public String getValue;
+
+
+    public int getValue() {
+        return value;
+    }
+
+    public void throwDie() {
+// static skal skride
+        Random rand = new Random();
+        // Numbers of sides on the Die is set below this line
+        value = rand.nextInt(6) + 1;
+    }
 }
