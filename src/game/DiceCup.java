717d7db3054096e542f80d7fe7d5cedfdc98efbf
Mads <hemermads@gmail.com>
2018-11-06 18:50:27 +0100

    Raflebæger

--- src/game/DiceCup.java
+++ src/game/DiceCup.java
@@ -1,4 +1,28 @@
 package game;
 
-public class DiceCup {
+public class DiceCup {private Die[] dice = {new Die(), new Die()};
+
+
+    public int getValue() {
+        int sum = 0;
+        for (Die d : dice) {
+            sum += d.getValue();
+        }
+        return sum;
+    }
+
+    public void printRolls() {
+        for (Die d : dice) {
+            System.out.print(d.getValue + " ");
+        }
+    }
+
+
+    public void throwDice() {
+        for (int i = 0; i < dice.length; i++) {
+            dice[i].throwDie();
+        }
+
+    }
 }
+
