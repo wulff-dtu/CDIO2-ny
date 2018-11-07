18d007b5d962d08eefdbddab0f50043cf1c8519a
Mads <hemermads@gmail.com>
2018-11-07 10:09:54 +0100

    mads commit

--- src/game/DiceCup.java
+++ src/game/DiceCup.java
@@ -23,33 +23,3 @@ public class DiceCup {
     }
 }
 
-//mads¨ forslag nedenunder
-
-public class DiceCup {
-
-private Die[] dice = {new Die(), new Die()};
-
-
-    public int getValue() {
-        int sum = 0;
-        for (Die d : dice) {
-            sum += d.getValue();
-        }
-        return sum;
-    }
-
-
-    public void printRolls() {
-        for (Die d : dice) {
-            System.out.print(d.getValue + " ");
-        }
-    }
-
-
-    public void throwDice() {
-        for (int i = 0; i < dice.length; i++) {
-            dice[i].throwDie();
-        }
-
-    }
-}
