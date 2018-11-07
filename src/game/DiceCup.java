1775482b9cb4ad9a1786ee88961bbc228080d103
wulff-dtu <s185036@student.dtu.dk>
2018-11-07 09:19:53 +0100

    Fjernede nu min kode i DiceCup.

--- src/game/DiceCup.java
+++ src/game/DiceCup.java
@@ -1,34 +1,4 @@
 package game;
-/*
-public class DiceCup {
-
-    private Die[] dice;
-    private int sum;
-
-    public DiceCup(int numOfDice) {
-        dice = new Die[numOfDice];
-        for(int i = 0; i < numOfDice; i++) {
-            dice[i] = new Die(6);
-        }
-    }
-
-    //TODO add throwDice method.
-
-    public void throwDice() {
-        int accumulateSum = 0;
-        for (Die die : dice) {
-            die.throwDie();
-            accumulateSum += die.getValue();
-        }
-        sum = accumulateSum;
-    }
-
-    public int getSum() {
-        return sum;
-    }
-}*/
-
-//mads¨ forslag nedenunder
 
 public class DiceCup {
 
