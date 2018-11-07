85b15baef29e08265f26067401d59c7f4b77e8b2
Mads <hemermads@gmail.com>
2018-11-07 10:26:27 +0100

    mads commit

--- src/game/DiceCup.java
+++ src/game/DiceCup.java
@@ -9,10 +9,12 @@ public class DiceCup {
         dice = new Die[numOfDice];
         for(int i = 0; i < numOfDice; i++) {
             dice[i] = new Die(6);
+            //TODO add constructor in Die class
         }
     }
 
-    //TODO add throwDice method.//har kastet en throwDice ind fra mit forslag(mads)
+    //TODO add throwDice method.
+    //committed a suggestion to the throwDice method(mads)
     public void throwDice() {
         for (int i = 0; i < dice.length; i++) {
             dice[i].throwDie();}
