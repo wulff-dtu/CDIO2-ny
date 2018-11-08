97a162031f26463d6a19acf33825ed199e2f1087
camillayir <43875752+camillayir@users.noreply.github.com>
2018-11-08 15:26:00 +0100

    Update Bankroll.java

--- src/game/Bankroll.java
+++ src/game/Bankroll.java
@@ -1,5 +1,29 @@
 package game;
 
 public class Bankroll {
-    /*commit again*/
+
+    private int balance;
+
+    //The game starts with each player having 1000 and the bankroll can't be negative
+
+    public void changeBalance(){
+
+        balance = 1000;
+        if(balance < 0 ){
+
+            balance = 0;
+        }
+
+    }
+
+    //Get balance
+
+    public Bankroll(){}
+
+    public int getBalance() {
+
+            return balance;
+    }    
+
+
 }
